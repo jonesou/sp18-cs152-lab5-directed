e58e1a8966ccfbf89ee12440ab6635c333f6fda9
Class Account <cs152-aaz@icluster6.EECS.Berkeley.EDU>
2018-04-27 06:15:15 -0700

    directed portion code

---
+++ vvadd.c
@@ -0,0 +1,28 @@
+// See LICENSE for license details.
+
+#include "stdlib.h"
+#include "dataset.h"
+
+//--------------------------------------------------------------------------
+// vvadd function
+
+void __attribute__((noinline)) vvadd(int coreid, int ncores, size_t n, const data_t* x, const data_t* y, data_t* z)
+{
+   size_t i;
+
+   // interleave accesses
+   for (i = coreid; i < n; i+=ncores)
+   {
+      z[i] = x[i] + y[i];
+   }
+}
+
+void __attribute__((noinline)) vvadd_opt(int coreid, int ncores, size_t n, const data_t* x, const data_t* y, data_t* z)
+{
+    size_t i, blockSize = n / ncores;
+
+    for (i = coreid * blockSize; i< (coreid+1) * blockSize; i+=1)
+    {
+        z[i] = x[i] + y[i];
+    }
+}
